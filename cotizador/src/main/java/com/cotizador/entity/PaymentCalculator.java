1b1032d6df2acddb74c187a48d5fd7bb8501539f
HugoSanche <hbmclaren@gmail.com>
2024-06-14 15:50:20 -0600

    add contructor and getters and setters to entity Payment calculator

--- cotizador/src/main/java/com/cotizador/entity/PaymentCalculator.java
+++ cotizador/src/main/java/com/cotizador/entity/PaymentCalculator.java
@@ -14,13 +14,20 @@ public class PaymentCalculator {
     @Column(name = "Date")
     private Date fechaCotizacion;
     @Column(name = "YearVehicle")
-    private Date yearVehicle;
+    private int yearVehicle;
     @Column(name = "VehiclePrice")
     private double vehiclePrice;
     @Column(name = "DownPayment")
     private double downPayment;
+
+
     @Column(name = "PersonId")
     private int personId;
+
+    @ManyToOne
+    @JoinColumn(name ="personId" )
+    private Individual individual;
+
     @Column(name = "BrandId")
     private int brandId;
     @Column(name = "ModelId")
@@ -31,6 +38,137 @@ public class PaymentCalculator {
     @Column(name = "Version")
     private int version;
 
+    public PaymentCalculator(  int yearVehicle, double vehiclePrice, double downPayment, int loanTerm) {
+        this.yearVehicle = yearVehicle;
+        this.vehiclePrice = vehiclePrice;
+        this.downPayment = downPayment;
+        this.loanTerm = loanTerm;
+    }
+
+    public PaymentCalculator(int paymentCalculatorId, Date fechaCotizacion, int yearVehicle, double vehiclePrice,
+                             double downPayment, int personId, Individual individual, int brandId, int modelId,
+                             int loanTerm, int version) {
+        this.paymentCalculatorId = paymentCalculatorId;
+        this.fechaCotizacion = fechaCotizacion;
+        this.yearVehicle = yearVehicle;
+        this.vehiclePrice = vehiclePrice;
+        this.downPayment = downPayment;
+        this.personId = personId;
+        this.individual = individual;
+        this.brandId = brandId;
+        this.modelId = modelId;
+        this.loanTerm = loanTerm;
+        this.version = version;
+    }
+
+
+    public PaymentCalculator() {
+    }
+
+    public int getPaymentCalculatorId() {
+        return paymentCalculatorId;
+    }
+
+    public void setPaymentCalculatorId(int paymentCalculatorId) {
+        this.paymentCalculatorId = paymentCalculatorId;
+    }
+
+    public Date getFechaCotizacion() {
+        return fechaCotizacion;
+    }
+
+    public void setFechaCotizacion(Date fechaCotizacion) {
+        this.fechaCotizacion = fechaCotizacion;
+    }
+
+    public int getYearVehicle() {
+        return yearVehicle;
+    }
+
+    public void setYearVehicle(int yearVehicle) {
+        this.yearVehicle = yearVehicle;
+    }
+
+    public double getVehiclePrice() {
+        return vehiclePrice;
+    }
+
+    public void setVehiclePrice(double vehiclePrice) {
+        this.vehiclePrice = vehiclePrice;
+    }
+
+    public double getDownPayment() {
+        return downPayment;
+    }
+
+    public void setDownPayment(double downPayment) {
+        this.downPayment = downPayment;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public Individual getIndividual() {
+        return individual;
+    }
+
+    public void setIndividual(Individual individual) {
+        this.individual = individual;
+    }
+
+    public int getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(int brandId) {
+        this.brandId = brandId;
+    }
+
+    public int getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(int modelId) {
+        this.modelId = modelId;
+    }
+
+    public int getLoanTerm() {
+        return loanTerm;
+    }
+
+    public void setLoanTerm(int loanTerm) {
+        this.loanTerm = loanTerm;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return "PaymentCalculator{" +
+                "paymentCalculatorId=" + paymentCalculatorId +
+                ", fechaCotizacion=" + fechaCotizacion +
+                ", yearVehicle=" + yearVehicle +
+                ", vehiclePrice=" + vehiclePrice +
+                ", downPayment=" + downPayment +
+                ", personId=" + personId +
+                ", individual=" + individual +
+                ", brandId=" + brandId +
+                ", modelId=" + modelId +
+                ", loanTerm=" + loanTerm +
+                ", version=" + version +
+                '}';
+    }
 }
 
 
