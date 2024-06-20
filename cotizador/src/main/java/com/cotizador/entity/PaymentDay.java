ce47694cff21d2fc17c0868c3b41ec01bf67b65d
HugoSanche <hbmclaren@gmail.com>
2024-06-19 19:22:29 -0600

    modify values for loadTerm

--- cotizador/src/main/java/com/cotizador/entity/PaymentDay.java
+++ cotizador/src/main/java/com/cotizador/entity/PaymentDay.java
@@ -1,2 +1,29 @@
-package com.cotizador.entity;public class PaymentDay {
+package com.cotizador.entity;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table
+public class PaymentDay {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PaymentDayId")
+    private int paymentDayId;
+    @Column(name = "PaymentDay")
+    private int paymentDay;
+    @Column(name = "Execute")
+    private boolean execute;
+    @Column(name = "Status")
+    private String status;
+    @Column(name = "LastUpdate")
+    private Date lastUpdate;
+    @Column(name = "Register")
+    private Date register;
+
 }
+
+
+
+
