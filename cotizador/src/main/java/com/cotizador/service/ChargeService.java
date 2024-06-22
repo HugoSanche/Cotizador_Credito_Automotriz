602432f76cff19b25807e11301050cb3c6d587dd
HugoSanche <hbmclaren@gmail.com>
2024-06-22 11:02:56 -0600

    Write code for charge service

---
+++ cotizador/src/main/java/com/cotizador/service/ChargeService.java
@@ -0,0 +1,14 @@
+package com.cotizador.service;
+
+import com.cotizador.entity.Charges;
+
+import java.util.List;
+
+public interface ChargeService {
+    List<Charges> findAll();
+    Charges findById(int theId);
+    List<Charges> findByName(String nameCharge);
+    void update(Charges charges);
+    void save(Charges charges);
+    void delete(int theId);
+}
---
+++ cotizador/src/main/java/com/cotizador/service/ChargeServiceImp.java
@@ -0,0 +1,41 @@
+package com.cotizador.service;
+
+import com.cotizador.dao.ChargesDAO;
+import com.cotizador.entity.Charges;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+
+public class ChargeServiceImp implements ChargeService{
+    ChargesDAO chargesDAO;
+    @Autowired
+    public ChargeServiceImp(ChargesDAO chargesDAO) {
+        this.chargesDAO = chargesDAO;
+    }
+    @Override
+    public List<Charges> findAll() {
+        return chargesDAO.findAll();
+    }
+    @Override
+    public Charges findById(int theId) {
+        return chargesDAO.findById(theId);
+    }
+    @Override
+    public List<Charges> findByName(String nameCharge) {
+        return chargesDAO.findByName(nameCharge);
+    }
+    @Override
+    public void update(Charges charges) {
+        chargesDAO.update(charges);
+    }
+    @Override
+    public void save(Charges charges) {
+        chargesDAO.save(charges);
+    }
+    @Override
+    public void delete(int theId) {
+        chargesDAO.delete(theId);
+    }
+}
