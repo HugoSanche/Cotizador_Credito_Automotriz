8266d38482af8b7e0dcc15fabf33d0114b8f333e
HugoSanche <hbmclaren@gmail.com>
2024-06-21 23:49:26 -0600

    implements DAO for charges

--- cotizador/src/main/java/com/cotizador/dao/ChargesDAO.java
+++ cotizador/src/main/java/com/cotizador/dao/ChargesDAO.java
@@ -1,2 +1,14 @@
-package com.cotizador.dao;public interface ChargesDAO {
+package com.cotizador.dao;
+
+import com.cotizador.entity.Charges;
+
+import java.util.List;
+
+public interface ChargesDAO {
+    List<Charges> findAll();
+    Charges findById(int theId);
+   List<Charges> findByName(String nameCharge);
+    void update(Charges charges);
+    void save(Charges charges);
+    void delete(int theId);
 }
--- cotizador/src/main/java/com/cotizador/dao/ChargesDAOImp.java
+++ cotizador/src/main/java/com/cotizador/dao/ChargesDAOImp.java
@@ -1,2 +1,52 @@
-package com.cotizador.dao;public class ChargesDAOImp {
+package com.cotizador.dao;
+
+import com.cotizador.entity.Charges;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ChargesDAOImp implements ChargesDAO{
+    EntityManager entityManager;
+
+    public ChargesDAOImp(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Charges> findAll() {
+        TypedQuery<Charges> charges=entityManager.createQuery("from Charges", Charges.class);
+        return charges.getResultList();
+    }
+
+    @Override
+    public Charges findById(int theId) {
+        Charges charges=entityManager.find(Charges.class,theId);
+        return charges;
+    }
+
+    @Override
+    public List<Charges> findByName(String nameCharge) {
+        TypedQuery<Charges> charges=entityManager.createQuery("from charges where name="+nameCharge, Charges.class);
+        return charges.getResultList();
+    }
+
+    @Override
+    public void update(Charges charges) {
+        entityManager.merge(charges);
+
+    }
+
+    @Override
+    public void save(Charges charges) {
+        entityManager.persist(charges);
+    }
+
+    @Override
+    public void delete(int theId) {
+        Charges charges=entityManager.find(Charges.class,theId);
+        entityManager.remove(charges);
+    }
 }
