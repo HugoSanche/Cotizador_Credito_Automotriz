764bab1ef3e1d8d3aa6da91a63777979817ac7fe
HugoSanche <hbmclaren@gmail.com>
2024-06-28 13:03:08 -0600

    get value of fixed rate from file properties

--- cotizador/src/main/java/com/cotizador/util/TestDate.java
+++ cotizador/src/main/java/com/cotizador/util/TestDate.java
@@ -1,20 +1,25 @@
 package com.cotizador.util;
 
-import java.time.LocalDate;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class TestDate {
     public static void main(String[] args) {
         // get a reference to today
-        LocalDate today = LocalDate.now();
+        LocalDateTime today = LocalDateTime.now();
         // having today,
-        LocalDate firstOfNextMonth = today
+        LocalDateTime firstOfNextMonth = today
                 // add one to the month
                 .withMonth(today.getMonthValue() + 1)
                 // and take the first day of that month
                 .withDayOfMonth(15);
         // then return it as formatted String
         System.out.println( (firstOfNextMonth.format(DateTimeFormatter.ISO_LOCAL_DATE)));
+
+        long daysBetween = Duration.between(today, firstOfNextMonth).toDays();
+        System.out.println ("Days: " + daysBetween);
+
 
     }
 }
