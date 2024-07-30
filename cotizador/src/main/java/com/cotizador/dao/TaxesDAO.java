99a8ea2e73dc4c1a3c8b7290a9e60f79f1e1af10
HugoSanche <hbmclaren@gmail.com>
2024-07-29 18:03:33 -0600

    modifiy taxes

--- cotizador/src/main/java/com/cotizador/dao/TaxesDAO.java
+++ cotizador/src/main/java/com/cotizador/dao/TaxesDAO.java
@@ -1,2 +1,14 @@
-package com.cotizador.dao;public interface TaxesDAO {
+package com.cotizador.dao;
+
+import com.cotizador.entity.Taxes;
+
+import java.util.List;
+
+public interface TaxesDAO {
+    List<Taxes> findAll();
+    Taxes findById(int theId);
+    Taxes findByName(String nameTaxe);
+    void update(Taxes taxes);
+    void save(Taxes taxes);
+    void delete(int theId);
 }
--- cotizador/src/main/java/com/cotizador/dao/TaxesDAOImp.java
+++ cotizador/src/main/java/com/cotizador/dao/TaxesDAOImp.java
@@ -1,2 +1,55 @@
-package com.cotizador.dao;public class TaxesDAOImp {
+package com.cotizador.dao;
+
+import com.cotizador.entity.Taxes;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class TaxesDAOImp implements TaxesDAO{
+    EntityManager entityManager;
+
+    @Autowired
+    public TaxesDAOImp(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Taxes> findAll() {
+        TypedQuery<Taxes> taxesTypedQuery=entityManager.createQuery(" from Taxes", Taxes.class);
+        return taxesTypedQuery.getResultList();
+    }
+
+    @Override
+    public Taxes findById(int theId) {
+        Taxes taxes=entityManager.find(Taxes.class,theId);
+        return taxes;
+    }
+
+    @Override
+    public Taxes findByName(String nameTaxes) {
+        TypedQuery<Taxes> taxes=entityManager.createQuery("from Taxes where name=:theData", Taxes.class);
+        taxes.setParameter("theData",nameTaxes);
+        return taxes.getResultList().get(0);
+    }
+
+    @Override
+    public void update(Taxes taxes) {
+        entityManager.merge(taxes);
+    }
+
+    @Override
+    public void save(Taxes taxes)
+    {
+        entityManager.persist(taxes);
+    }
+
+    @Override
+    public void delete(int theId) {
+        Taxes taxes=entityManager.find(Taxes.class,theId);
+        entityManager.remove(taxes);
+    }
 }
